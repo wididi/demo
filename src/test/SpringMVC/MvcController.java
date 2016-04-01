50fbde2a46be694e2e269c7422060cde5d5cb1d5
liudi <liudi@192.168.1.21>
2016-04-01 18:06:33 +0800

    dsa

---
+++ src/test/SpringMVC/MvcController.java
@@ -0,0 +1,21 @@
+package test.SpringMVC;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mvc")
+public class MvcController 
+{
+	@RequestMapping("/hello")
+    public String hello(){        
+        return "hello";
+    }
+	
+	@RequestMapping("/person")
+	public String toPerson(String name,double age)
+	{
+		System.out.println(name+" "+age);
+		return "hello";
+	}
+}
